608a1d215258841654ae75d9b47fc5b4d550f619
gongxudong <58641378@qq.com>
2025-05-27 08:37:26 +0800

    指定主类扫描路径

--- src/main/java/org/example/springboot/SpringbootApplication.java
+++ src/main/java/org/example/springboot/SpringbootApplication.java
@@ -3,7 +3,7 @@ package org.example.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.example.springboot")
 public class SpringbootApplication {
 
     public static void main(String[] args) {
